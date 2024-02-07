7c06baadc0474b45068cacdba8528296d502665f
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2024-02-07 17:48:59 +0000

    apply simple password protection to help guard against bots & add spinner to input button

--- src/components/MessageInput.tsx
+++ src/components/MessageInput.tsx
@@ -1,4 +1,10 @@
-import { Box, Flex, Input, InputGroup, InputRightElement} from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
 import { IconButton } from "@chakra-ui/react";
 import { ArrowDownIcon } from "@chakra-ui/icons";
 import { ChangeEvent, useEffect, useState } from "react";
@@ -6,67 +12,74 @@ import { ChangeEvent, useEffect, useState } from "react";
 interface Props {
   textInput: string;
   userRequestSent: (value: string) => void;
-  onDataReceived: (data: {role: string, content: string}) => void;
+  onDataReceived: (data: { role: string; content: string }) => void;
 }
 
-const MessageInput = ({ textInput, userRequestSent, onDataReceived}: Props) => {
-
+const MessageInput = ({
+  textInput,
+  userRequestSent,
+  onDataReceived,
+}: Props) => {
   const [input, setInput] = useState<string>(textInput);
 
-  // update local state when initalValue changes 
+  // update local state when initalValue changes
   useEffect(() => {
-    setInput(textInput)
-  }, [textInput,]);
-  
-  
-   // function that handles updating the local state to set the input on the message property
-   // in the fetch call to Open API and set the input on the onTextChange to pass to parent component
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-      const newValue = e.target.value;
-      setInput(newValue);
-      userRequestSent(newValue);      
+    setInput(textInput);
+  }, [textInput]);
+
+  // function that handles updating the local state to set the input on the message property
+  // in the fetch call to Open API and set the input on the onTextChange to pass to parent component
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setInput(newValue);
+    userRequestSent(newValue);
+  };
+
+  // load spinner on icon button when clicked
+  const [showLoader, setShowLoader] = useState(false);
+
+  const getMessage = async () => {
+    setShowLoader(true);
+
+    const messageResponseCountLimit = ".Please cap message to 80 words max";
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: input.concat(messageResponseCountLimit),
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     };
 
-    const getMessage = async () => {
-
-      const messageResponseCountLimit = '.Please cap message to 80 words max'
-      const options = {
-        method: "POST",
-        body: JSON.stringify({
-          message: input. concat(messageResponseCountLimit)
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-
-      try {
-        const response = await fetch(
-          "http://localhost:8000/completions",
-          options
-        );
-        const data = await response.json();
-        onDataReceived(data.choices[0].message);
-        setInput("");
-      } catch (error) {
-        console.error(error);
-      }
-    };
+    try {
+      const response = await fetch(
+        "http://localhost:8000/completions",
+        options
+      );
+      const data = await response.json();
+      onDataReceived(data.choices[0].message);
+      setInput("");
+      setShowLoader(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
-     <Flex p={3} mt={4} borderRadius={15} bg='#757575'>
+      <Flex p={3} mt={4} borderRadius={15} bg="#757575">
         <Box p={[0.5, 2]} w={[250, 400, 600, 800, 1000]}>
-          <InputGroup>
-            <InputRightElement p={25} pt={{base:"16px" , md:"22px"}} pr={{base:"16px", md:"24px"}}>
+          <InputGroup size="sm">
+            <InputRightElement p={[17, 25]}>
               <IconButton
                 icon={<ArrowDownIcon />}
                 size={["xs", "sm"]}
                 colorScheme="#757575"
                 variant="outline"
                 aria-label={"Submit message"}
-                onClick={getMessage}>
-              </IconButton>
+                onClick={getMessage}
+                isLoading={showLoader}></IconButton>
             </InputRightElement>
             <Input
               value={input}
@@ -74,9 +87,9 @@ const MessageInput = ({ textInput, userRequestSent, onDataReceived}: Props) => {
               placeholder="Write me a birthday message for my..."
               size={["sm", "lg"]}
               textOverflow="wrap"
-            />   
+            />
           </InputGroup>
-        </Box>           
+        </Box>
       </Flex>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
+var password = "iAmHuman"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
-)
+if(prompt("To protect against bots, please enter password to access app:", "") === password) 
+  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>
+  );
+else{
+  alert('Sorry, wrong password!');
+}
