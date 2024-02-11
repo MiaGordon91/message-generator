f22ed97847cdc5ec6194ca38a749a24d3654ac02
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2024-02-11 16:22:52 +0000

    moved server to dedicated folder

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-var password = "iAmHuman"
+var password = "iAmHuman";
 
-if(prompt("To protect against bots, please enter password to access app:", "") === password) 
-  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </React.StrictMode>
-  );
-else{
-  alert('Sorry, wrong password!');
-}
+// if(prompt("To protect against bots, please enter password to access app:", "") === password)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
+// else{
+//   alert('Sorry, wrong password!');
+// }
