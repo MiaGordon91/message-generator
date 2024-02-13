fed787c6407f92b26f83107871c28b3b41aaf4c3
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2024-02-13 18:13:17 +0000

    attempt workaround to allow all origins

--- server/server.tsx
+++ server/server.tsx
@@ -22,7 +22,7 @@ app.use(express.json());
 
 //allow requests from react frontend to avoid CORS policy restriction
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", `${REACT_APP_ROUTE}`);
+    res.header("Access-Control-Allow-Origin", `'*'`);
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
