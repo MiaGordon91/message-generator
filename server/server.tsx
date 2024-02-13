b50d25421e7e51ab804c10d6484bd4053e23d5a4
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2024-02-13 17:00:18 +0000

    attempt to enable CORS across client and server URL

--- server/server.tsx
+++ server/server.tsx
@@ -1,14 +1,14 @@
 export {};
-import path from 'path';
-import dotenv from 'dotenv';
+import path from "path";
+import dotenv from "dotenv";
 
 //points file to access .env in root directory
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
-const pathToEnv = path.resolve(__dirname, '../.env');
-dotenv.config({path: pathToEnv});
+const pathToEnv = path.resolve(__dirname, "../.env");
+dotenv.config({ path: pathToEnv });
 
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const PORT = 8000;
 const app = express();
@@ -19,43 +19,59 @@ app.use(express.json());
 
 app.use(cors());
 
-const VITE_API_KEY = process.env.OPEN_API_KEY;
+const OPEN_API_KEY = process.env.OPEN_API_KEY;
 const EXPRESS_ROUTE = process.env.EXPRESS_ROUTE;
+const REACT_APP_ROUTE = process.env.REACT_APP_ROUTE;
+
+//update header to fetch data and avoid “No Access-Control-Allow-Origin header”
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", `${REACT_APP_ROUTE}`);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 
 //define the express route
 // with asyn function with await keyword that gets the response
-app.post(`${EXPRESS_ROUTE}`, async(req: any, res: { send: (arg0: any) => void; }) => {
-
+app.post(
+  `${EXPRESS_ROUTE}`,
+  async (req: any, res: { send: (arg0: any) => void }) => {
     // options defines what kind of request this is going to be
     const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${VITE_API_KEY}`,
-        },
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [
-                {
-                    role: "user",
-                    content: req.body.message
-                }
-            ],
-            max_tokens: 100,
-        })   
-    }
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${OPEN_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: req.body.message,
+          },
+        ],
+        max_tokens: 100,
+      }),
+    };
 
-
-    try{
+    try {
       // post request to this URL and wait for a response to come back
-      const response = await fetch('https://api.openai.com/v1/chat/completions', options)  
-      const data = await response.json()
-    
+      const response = await fetch(
+        "https://api.openai.com/v1/chat/completions",
+        options
+      );
+      const data = await response.json();
+
       //send data to localhost route
-      res.send(data)
-    }catch(error){
-        console.error(error)
+      res.send(data);
+    } catch (error) {
+      console.error(error);
     }
-})
+  }
+);
 
 app.listen(PORT, () => console.log("Your server is running on PORT " + PORT));
