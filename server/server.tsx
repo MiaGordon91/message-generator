9144cfe7d5018aff7d7e1f0389df1332abcc632f
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2024-02-14 10:12:00 +0000

    CORS policy still an issue, revert back to default config

--- server/server.tsx
+++ server/server.tsx
@@ -24,7 +24,7 @@ app.use(cors());
 
 //allow requests from react frontend to avoid CORS policy restriction
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", `${REACT_APP_ROUTE}`);
+  res.header("Access-Control-Allow-Origin",`'*'`);
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
