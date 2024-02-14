73fe5781cf9d9a742f5cc843f2fe0b53b77992eb
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2024-02-14 10:49:03 +0000

    update with allowed methods

--- server/server.tsx
+++ server/server.tsx
@@ -24,10 +24,13 @@ app.use(cors());
 
 //allow requests from react frontend to avoid CORS policy restriction
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin",'*');
+  res.header("Access-Control-Allow-Origin","*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header(
+    "Access-Control-Allow-Methods","PUT, GET, POST, DELETE, OPTONS"
   );
   next();
 });
