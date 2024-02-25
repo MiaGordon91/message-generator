f2ef595d3554f794761432f1b9f097a1b0293909
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2024-02-25 20:58:34 +0000

    install webpack and babel to compile ts into js that Vercel can deploy

---
+++ api/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: 'node',
+  entry: './index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: {
+        type: 'module'
+    },
+    chunkFormat: 'module'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          },
+          'ts-loader'
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+    resolve: {
+        extensions: ['.tsx', '.ts' ,'.js']
+},
+    externals: [nodeExternals()],
+    experiments: {
+        outputModule: true
+    }    
+};
